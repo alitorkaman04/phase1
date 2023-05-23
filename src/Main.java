4bf2f05b4febf9a9012fd07d8f2ff519b18313e4
alitorkaman04 <alitorkaman1383@gmail.com>
2023-05-23 16:11:05 +0430

    checking

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        InputOutputProcessor inputOutput = InputOutputProcessor.getInstance();
+        Scanner sc = inputOutput.getScanner();
+
+        while (true) {
+            String[] dastoor = sc.nextLine().split(" ");
+            if(dastoor[0].equals("exit"))
+                break;
+            else if(dastoor[0].equals("ADD") && dastoor[1].equals("USER"))
+                User.addNewUser(dastoor[3], dastoor[2]);
+
+            else
+                System.out.println(CheckResult.INVALID_COMMAND);
+
+//TODO : Checking
+        }
+
     }
 }
